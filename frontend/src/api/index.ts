e96574fb5d2cc312e7986ca690c40e70dfc7a0f7
Lucas Britto <mister.britto@hotmail.com>
2025-10-24 14:59:57 -0300

    refactor: Ambiente de variavel para propução e local

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import type { InternalAxiosRequestConfig } from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_URL,
 })
 
 api.interceptors.request.use(
